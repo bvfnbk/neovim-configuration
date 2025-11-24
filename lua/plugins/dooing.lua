9e5da6e4e259e99c828586be19c85ba6c6a0f144
bvfnbk <bvfnbk@>
2025-11-24 15:23:07 +0100

    Updates window size, disables quick help window

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -1,6 +1,12 @@
 return {
   "atiladefreitas/dooing",
   config = function()
-    require("dooing").setup({})
+    require("dooing").setup({
+      window = {
+        width = 100,
+        height = 50
+      },
+      quick_keys = false
+    })
   end
 }
