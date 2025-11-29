d73ce383bfeabc84467990730e4cf95985db306b
bvfnbk <bvfnbk@>
2025-11-29 16:47:31 +0100

    Experimented with Noice configuration (to enable Warning messages); to no avail

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,13 +6,25 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify"
   },
-  config = function()  
+  config = function()
     require("noice").setup({
       lsp = {
+        progress = {
+          enabled = true
+        },
+        hover = {
+          enabled = true
+        },
+        signature = {
+          enabled = true
+        },
+        message = {
+          enabled = true,
+        },
         override = {
-          [ "vim.lsp.util.convert_input_to_markdown_lines" ] = true,
-          [ "vim.lsp.util.stylize_markdown" ] = true,
-          [ "cmp.entry.get_documentation" ] = true
+          [ "vim.lsp.util.convert_input_to_markdown_lines" ] = false,
+          [ "vim.lsp.util.stylize_markdown" ] = false,
+          [ "cmp.entry.get_documentation" ] = false
         }
       },
       presets = {
@@ -20,7 +32,7 @@ return {
         command_palette = true,
         long_message_to_split = true,
         inc_rename = false,
-        lsp_doc_border = false
+        lsp_doc_border = true
       }
     })
   end
