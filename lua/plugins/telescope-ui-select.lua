51b7f632f4e37b7b34fb3df02da18e44c80bebbf
bvfnbk <bvfnbk@>
2025-12-15 12:47:52 +0100

    Switched to opts: telescope/telescope-ui-select

--- lua/plugins/telescope-ui-select.lua
+++ lua/plugins/telescope-ui-select.lua
@@ -1,6 +1,17 @@
 return {
   "nvim-telescope/telescope-ui-select.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
+  dependencies = { 
+    "nvim-telescope/telescope.nvim" 
+  },
+  opts = function (_, opts)
+    local themes = require("telescope.themes")
+
+    opts.extensions = opts.extensions or {}
+    opts.extensions["ui-select"] = themes.get_dropdown({})
+  end,
+  config = function (_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("ui-select")
   end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,18 +3,5 @@ return {
   tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim"
-  },
-  config = function()
-    require("telescope").setup({
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown({
-
-          })
-        }
-      }
-    })
-
-    require("telescope").load_extension("ui-select")
-  end
+  }
 }
