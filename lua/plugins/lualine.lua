80d2c681b757baa589c75fa087f599b7478b2aba
bvfnbk <bvfnbk@>
2025-12-15 12:20:13 +0100

    Switched to opts: lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
+  opts = function(_, opts)
+    opts.theme = 'dracula'
   end
 }
 
