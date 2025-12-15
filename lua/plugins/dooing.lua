fd46ca0550cff1455a5de0769b779480d8bdecfc
bvfnbk <bvfnbk@>
2025-12-15 12:19:09 +0100

    Switched to opts: dooing.lua

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -1,12 +1,11 @@
 return {
   "atiladefreitas/dooing",
-  config = function()
-    require("dooing").setup({
-      window = {
-        width = 100,
-        height = 50
-      },
-      quick_keys = false
-    })
+  opts = function(_, opts)
+    opts.window = {
+      width = 100,
+      height = 50
+    }
+
+    opts.quick_keys = false
   end
 }
