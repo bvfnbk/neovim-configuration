0131a8f39731c6ce28c5e0aa6f69b22b888708d3
bvfnbk <bvfnbk@>
2025-12-15 14:11:18 +0100

    Adds capabilities

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,27 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
+    local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    vim.lsp.config("lua_ls", {
+      capabilities = default_capabilities,
+      filetypes = { "lua" },
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = {
+              "vim"
+            }
+          }
+        }
+      }
+    })
+
+    vim.lsp.config("marksman", {
+      capabilities = default_capabilities,
+      filetypes = { "markdown" }
+    })
+
     vim.lsp.enable("lua_ls")
     vim.lsp.enable("marksman")
   end
